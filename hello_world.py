5e009afaf1fe20fdc24846ad32a935ed194eedc1
Ryan Donlan <44676500+rdonlan@users.noreply.github.com>
2021-02-17 14:22:36 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-print("hello world")
+"""
+Our first Python test program.
+Team members: Ryan Donlan, Caleb Eurich, Noah Gans
+"""
+
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
